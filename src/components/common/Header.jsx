e439c1dd43429f0f12993e02373c20836dc8d64b
SantiagoRensonnet <63661270+SantiagoRensonnet@users.noreply.github.com>
2022-08-20 00:13:02 -0300

    Add Responsive Burger Menu

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -6,12 +6,18 @@ import { Link } from "react-router-dom";
 import SearchBar from "../utils/SearchBar";
 //Styles
 import "../../css/common/Header.css";
+import { useState } from "react";
 
 export default function Header({
   token,
   isSearchBarSelected,
   setIsSearchBarSelected,
 }) {
+  const [burgerActive, setBurgerActive] = useState(false);
+
+  function toggleBurger() {
+    setBurgerActive(burgerActive ? false : true);
+  }
   return (
     <div className="header">
       <div className="header--title-container">
@@ -22,18 +28,33 @@ export default function Header({
         )}
         <h2 className="header--title">Alkemy Challenge</h2>
       </div>
-      <nav className="header--navbar">
-        <Link to="/">Home</Link>
-        <div className="header--trending-container">
-          <img
-            src={trendingLogo}
-            alt="trending"
-            className="header--trending-logo"
-          />
-          <Link to="trending">Trending</Link>
+      <nav className={`header--navbar `}>
+        <div className={`header--nav-menu ${burgerActive ? "active" : ""}`}>
+          <Link to="/" className="header--nav-item">
+            Home
+          </Link>
+          <div className="header--trending-container header--nav-item">
+            <img
+              src={trendingLogo}
+              alt="trending"
+              className="header--trending-logo"
+            />
+            <Link to="trending">Trending</Link>
+          </div>
+          <Link to="favorites" className="header--nav-item">
+            Favorites
+          </Link>
         </div>
-        <Link to="favorites">Favorites</Link>
+        <div
+          className={`header--hamburger-menu ${burgerActive ? "active" : ""}`}
+          onClick={toggleBurger}
+        >
+          <span className="hamburger-bar"></span>
+          <span className="hamburger-bar"></span>
+          <span className="hamburger-bar"></span>
+        </div>
       </nav>
+
       <SearchBar
         isSearchBarSelected={isSearchBarSelected}
         setIsSearchBarSelected={setIsSearchBarSelected}
